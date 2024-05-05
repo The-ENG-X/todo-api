319fa717c0e5f6b18075da43d37466c376430305
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-05-05 22:03:50 +0200

    edit post method

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/tasks', (req, res) => {
 
 app.post('/tasks', (req, res) => {
   const newTask = req.body;
+  newTask.id = tasks.length + 1;
   tasks.push(newTask);
   res.status(201).json(newTask);
 });
