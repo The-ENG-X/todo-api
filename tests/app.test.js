66b07995268fa9e74cd8baf2c78e0cbd60ece854
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-05-05 21:48:05 +0200

    correct the index.js path

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./index');
+const app = require('../index');
 
 describe('Todo API endpoints', () => {
   it('should create a new task', createTaskTest);
